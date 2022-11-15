18b5edb8327dc78c2445ef708e825aabcfbfb230
Karigen B <82486699+Karigen@users.noreply.github.com>
2022-11-15 23:42:00 +0800

    fix: ...

---
+++ myblog-backend/src/test/java/me/myblog/PushTest.java
@@ -0,0 +1,12 @@
+package me.myblog;
+
+import me.myblog.backend.MyBlogBackendApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author Karigen B
+ * @create 2022-11-15 23:43
+ */
+@SpringBootTest(classes = MyBlogBackendApplication.class)
+public class PushTest {
+}
